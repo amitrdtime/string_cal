2db3cc23e242b329d01cbb82d780bf1c2b3fac1c
Amit <amit.deshmukh1014@gmail.com>
2025-04-10 12:33:51 +0530

    Made changes in code with adding feature to seperate delimiter begginig the string and filter it from the string and calcuate the result

--- string_calculator.js
+++ string_calculator.js
@@ -1,22 +1,33 @@
 // add function to calculate String number
 const add = (str) => {
     resultSum = 0;
+    delimiter = ","
 
     if(str === ""){
         return 0;
     }
 
-    str = str.replace(/\n/g, ",");
+    if (str.startsWith("//")) {
+        const parts = str.split("\n");
+        delimiter = parts[0].substring(2); 
+        str = parts[1]; 
+    }
 
-    let numberArray = str.split(','); 
+    str = str.replace(/\n/g, delimiter);
+
+    let numberArray = str.split(delimiter); 
+    
     if(numberArray.length > 0){
         for (num of numberArray) {
             resultSum = resultSum + parseInt(num);
         }
     }
+
+    
     return resultSum;
 }
 
 console.log("Sum:",add(""));
 console.log("Sum:",add("1,2"));
 console.log("Sum:",add("1\n2,3"));
+console.log("Sum:", add("//;\n1;2")); 
