311c5cdfc4c60f3ab399fc7a366c3da1416aba23
Amit <amit.deshmukh1014@gmail.com>
2025-04-10 12:08:15 +0530

    Made some changes to calculate string number with comma seperater and return sum of number

--- string_calculator.js
+++ string_calculator.js
@@ -1,8 +1,19 @@
 // add function to calculate String number
 const add = (str) => {
+    resultSum = 0;
+    
     if(str === ""){
         return 0;
     }
+
+    let numberArray = str.split(','); 
+    if(numberArray.length > 0){
+        for (num of numberArray) {
+            resultSum = resultSum + parseInt(num);
+        }
+    }
+    return resultSum;
 }
 
 console.log(add(""));
+console.log(add("1,2"));
